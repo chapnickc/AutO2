f055bf8a28f8f3980c256cac2a2a68192e56cda8
chapnickc <chapnickc@gmail.com>
2016-04-04 21:19:00 -0500

    test directory

---
+++ test/motor_control_test.py
@@ -0,0 +1,42 @@
+
+import RPi.GPIO as GPIO
+from time import sleep
+
+GPIO.setmode(GPIO.BCM)
+
+#Declare I/O
+step = 18
+enable =  25
+direction = 23
+freq = 1000         # lowest frequency
+
+GPIO.setup(step, GPIO.OUT)
+GPIO.setup(enable, GPIO.OUT)
+GPIO.setup(direction, GPIO.OUT)
+
+#Function
+GPIO.output(enable, False) # or GPIO.HIGH instead of True 
+
+# set the pwm to frequency to 1 kHz
+pwm = GPIO.PWM(step, freq)
+
+# sets the output to a 50% duty cycle
+pwm.start(50)
+
+for flip in range(3):
+    GPIO.output(direction, True)
+    sleep(0.05)
+    GPIO.output(direction, False)
+    sleep(0.05)
+
+#sleep(5.5)
+#sleep(10)
+
+# turn off power to the pin , does not put low 
+pwm.stop()
+
+# turn off the enable pin  and the step pin
+GPIO.output(step, False)
+GPIO.output(enable, True)
+
+GPIO.cleanup()
