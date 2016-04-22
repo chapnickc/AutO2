40427a514e533d12138aae2282300ae37333059b
piliya1 <iliya.peter@gmail.com>
2016-04-22 16:55:23 -0500

    Update and rename Wean.py to wean.py

---
+++ main/wean.py
@@ -0,0 +1,55 @@
+import RPi.GPIO as GPIO
+import time
+import matplotlib.pyplot as plt
+from kivy.garden.matplotlib.backend_kivyagg import FigureCanvas
+
+class wean:
+
+    """
+    To take user-specified parameters for patient SpO2 weaning. 
+    Parameters are inputs to the class when called in app.py
+    """
+    
+    def __init__(self, SpO2_high = 98, SpO2_low = 92, flow_start = 3, delt_flow = 0.5, delt_Tstep = 20):
+        self.SpO2_high = SpO2_high
+        self.SpO2_low = SpO2_low
+        self.flow_start = flow_start
+        self.delt_flow = delt_flow
+        self.delt_Tstep = delt_Tstep
+        
+    def showWean(self):
+        
+        #Build Figure
+        fig = plt.figure()
+        ax1 = fig.add_subplot(1,1,1) 
+        ax2 = ax1.twinx()
+
+        ax1.set_ylabel('O2 LPM')
+        ax2.set_ylabel(r'SpO$_2$')
+        wid = FigureCanvas(fig)
+        
+        fig.canvas.mpl_connect('axes_enter_event', enter_axes)
+        
+        #Calculate Ideal Piecewise Wean
+        wean_values = list()
+        time_values = list()
+        drops = int(self.flow_start/self.delt_flow)
+        for k in range(drops):
+            wean_values.append(self.flow_start - k*self.delt_flow)
+            wean_values.append(self.flow_start - k*self.delt_flow)
+            time_values.append(k*self.delt_Tstep)
+            time_values.append((k+1)*self.delt_Tstep)
+            
+        wean_values.append(0)
+        wean_values.append(0)
+        time_values.append((drops+1)*self.delt_Tstep)
+        time_values.append((drops+2)*self.delt_Tstep)
+        
+        #Plot Ideal Wean
+        self.ax1.clear()
+        self.ax1.plot(time_values, wean_values, linewidth = 3)
+
+        #plt.axis([len(fr.values)-200, len(fr.values)+10, min(fr.values)-5, max(fr.values) + 2])
+        self.ax1.figure.canvas.draw()
+      
+    #def startWean(self):
