0454304e867633ff9fe78ef7ded81ad0afd0a347
chapnickc <chapnickc@gmail.com>
2016-06-04 17:44:55 -0500

    -

--- server.py
+++ server.py
@@ -3,25 +3,24 @@ import socket
 
 def send_data(data_file, connection):
 	"""
-	Try to open the data file and send it to the client, 
+	Try to open the data file and send it to the client,
 	if not, print an error to the screen
-	"""	
+	"""
 	try:
 		f = open(data_file, 'r')
 	except IOError as e:
 		print ('Received an error of: {}'.format(e))
-	else:	    
+	else:
 		raw_data = f.readlines()
 		for line in raw_data:
 			connection.send(bytes(line, 'UTF-8'))
-
 		f.close()
 
 	print ('File Sent!')
 
 def main():
 	"""
-	Accept connections from clients and send them 
+	Accept connections from clients and send them
 	the test file.
 	"""
 	host = ''
@@ -33,7 +32,7 @@ def main():
 		s.listen(5)
 		conn, addr = s.accept()
 		print ('Connected to: {}'.format(addr))
-		data_file = './testfile2.txt'
+		data_file = './testdata.txt'
 		send_data(data_file, conn)
 
 if __name__ == '__main__':
